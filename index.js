5185a4ce975b7a66c66682d7cdb4ff418468cd93
Larry Hatfield <larry@themackhouse.com>
2023-05-11 21:44:16 -0400

    2nd change to server node-ex

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
+app.use(express.json()); // Required to parse JSON request body
+
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+app.post("/messages", (req, res) => {
+  const message = req.body.message;
+  console.log("Received message:", message);
+
+  // Here you can process the message, store it, or perform any other actions
+
+  res.send("Message received!");
 });
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log('Server is running...');
+  console.log("Server is running...");
 });
