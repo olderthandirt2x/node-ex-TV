f2620bf66a31108d85cd0111131194677c128b50
Larry Hatfield <larry@themackhouse.com>
2023-05-13 16:25:09 -0400

    commit number 3

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 
 app.use(express.json()); // Required to parse JSON request body
 
+// Array to store messages
+const messages = [];
+
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
@@ -11,9 +14,17 @@ app.post("/messages", (req, res) => {
   const message = req.body.message;
   console.log("Received message:", message);
 
-  // Here you can process the message, store it, or perform any other actions
+  // Store the message in the messages array
+  messages.push(message);
 
   res.send("Message received!");
+});
+
+app.get("/messages/latest", (req, res) => {
+  // Retrieve the latest message from the messages array
+  const latestMessage = messages[messages.length - 1];
+
+  res.json({ message: latestMessage });
 });
 
 app.listen(process.env.PORT || 3000, () => {
