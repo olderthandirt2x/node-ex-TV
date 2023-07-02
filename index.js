6c8a2403b2dd55ff6f5d97d0a09ecb6c027481dd
Larry Hatfield <larry@themackhouse.com>
2023-07-02 18:18:53 -0400

    added mongoose schema

--- index.js
+++ index.js
@@ -17,7 +17,21 @@ mongoose
   .then(() => console.log("Connected to MongoDB with .env"))
   .catch(err => console.error("Could not connect to MongoDB:", err));
 
-// Array to store messages
+// Define a schema for your collection
+const movieSchema = new mongoose.Schema({
+  adult: Boolean,
+  id: Number,
+  original_title: String,
+  popularity: Number,
+  video: Boolean,
+});
+
+// Create a model using the schema
+const Movie = mongoose.model("movie3", movieSchema);
+
+
+
+  // Array to store messages
 const messages = [];
 
 app.get("/", (req, res) => {
@@ -27,6 +41,21 @@ app.get("/", (req, res) => {
 app.post("/messages", (req, res) => {
   const message = req.body.message;
   console.log("Received message:", message);
+
+    try {
+      // Fetch movies from the database and sort by popularity in descending order
+      const movies = await Movie.find().sort({ popularity: -1 });
+  
+      res.json(movies);
+      
+      console.log(movies);
+
+    } catch (error) {
+      console.error("Error fetching movies:", error);
+      res.status(500).json({ error: "An error occurred while fetching movies" });
+    }
+  });
+  
 
   // Store the message in the messages array
   messages.push(message);
